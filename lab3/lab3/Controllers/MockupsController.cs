4e51df512e42cfc7f09a74e38b1de0edf6c510f5
Александра Семашко <Alexandrasv18@mail.ru>
2020-06-17 23:08:39 +0700

    Initial commit

---
+++ lab3/lab3/Controllers/MockupsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using lab3.Models;
+
+namespace lab3.Controllers
+{
+    public class MockupsController : Controller
+    {
+        private static List<Mockups> list = new List<Mockups>();
+        private static int rightans = -1;
+
+        public IActionResult Quiz(Mockups cla)
+        {
+            if (ModelState.IsValid) { rightans++; }
+            list.Add(cla);
+            if (Request.Method == "POST") { ViewBag.ans = rightans; list.RemoveAt(0); return View("~/Views/Mockups/Result.cshtml", list); }
+            var question = new Mockups(true);
+
+            return View(question);
+        }
+
+        public IActionResult Result()
+        {
+            ViewBag.ans = rightans;
+            return View(list);
+        }
+
+    }
+}
---
+++ lab3/lab3/Models/Mockups.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab3.Models
+{
+    [RightAns]
+    public class Mockups
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public int oper { get; set; }
+        public int ans { get; set; }
+
+        public Mockups()
+        {
+
+        }
+
+        public Mockups(bool a)
+        {
+            Random ran = new Random();
+            x = ran.Next(0, 10);
+            y = ran.Next(0, 10);
+            oper = ran.Next(0, 3);
+        }
+
+        public int DoOper()
+        {
+            switch (oper)
+            {
+                case 0: return Add();
+                case 1: return Sub();
+                case 2: return Mult();
+                case 3: return Div();
+                default: return 0;
+            }
+        }
+
+        public int Add() { return x + y; }
+        public int Sub() { return x - y; }
+        public int Mult() { return x * y; }
+
+        public int Div()
+        {
+            if (y == 0) { return 0; }
+            else { return x / y; }
+        }
+    }
+
+    public class RightAns : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            Mockups cl = value as Mockups;
+
+            if (cl.ans == cl.DoOper()) { return true; }
+
+            return false;
+        }
+    }
+}
